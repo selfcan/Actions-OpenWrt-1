0c83a70c07f20937b7059055a95bd8a18e2d7d6f
hy5528 <95360525+hy5528@users.noreply.github.com>
2022-03-17 09:45:36 +0800

    Create B70.sh

---
+++ diy/B70.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/Bootstrap/Argon/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 修改内核版本为5.10
+# sed -i 's/5.4/5.10/g' target/linux/ramips/Makefile
