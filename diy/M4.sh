d43a565480243a9bcea9bf1019eac5914598aac3
hy5528 <95360525+hy5528@users.noreply.github.com>
2022-03-17 20:56:45 +0800

    Update M4.sh

--- diy/M4.sh
+++ diy/M4.sh
@@ -14,4 +14,4 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 sed -i 's/Bootstrap/Argon/g' feeds/luci/collections/luci/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
