bdb9916c14fa38ae211db79315f46dd6fe18f17f
hy5528 <95360525+hy5528@users.noreply.github.com>
2022-04-03 06:54:15 +0800

    Update M4diy1.sh

--- M4diy1.sh
+++ M4diy1.sh
@@ -16,10 +16,11 @@
 
 # 添加插件源码
 # sed -i '$a src-git ddnsto https://github.com/linkease/ddnsto-openwrt' feeds.conf.default
-# sed -i '$a src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' feeds.conf.default
+sed -i '$a src-git argon https://github.com/jerrykuku/luci-theme-argon.git' feeds.conf.default
 # sed -i '$a src-git dnsfilter https://github.com/garypang13/luci-app-dnsfilter' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 # git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
+git clone 
 
 # git clone https://github.com/Zxilly/UA2F package/UA2F
 
