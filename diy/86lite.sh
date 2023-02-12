ef90e83eceb137d051082907d3fc470f0e5d3782
selfcan <99958756+selfcan@users.noreply.github.com>
2023-02-12 16:49:23 +0800

    Update 86lite.sh

--- diy/86lite.sh
+++ diy/86lite.sh
@@ -17,12 +17,12 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/f
 # Modify X86 Kernel 6.1
 sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' ./target/linux/x86/Makefile
 # 重新添加 luci-app-adbyby-plus-lite
-#rm -rf feeds/luci/applications/luci-app-adbyby-plus
-#git clone https://github.com/selfcan/luci-app-adbyby-plus-lite.git package/luci-app-adbyby-plus
+rm -rf feeds/luci/applications/luci-app-adbyby-plus
+git clone https://github.com/selfcan/luci-app-adbyby-plus-lite.git package/luci-app-adbyby-plus
 #添加luci-app-netspeedtest
 git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
 #添加 luci-app-my-dnshelper
-#git clone https://github.com/selfcan/openwrt-my-dnshelper.git package/luci-app-my-dnshelper
+git clone https://github.com/selfcan/openwrt-my-dnshelper.git package/luci-app-my-dnshelper
 #添加luci-app-filebrowser
 git clone https://github.com/tcsr200722/luci-app-filebrowser.git package/luci-app-filebrowser
 #git clone https://github.com/openwrt-xiaomi/luci-app-filebrowser.git package/luci-app-filebrowser
