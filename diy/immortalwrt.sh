e7c8506d614b4661d1c48b44a176ef3325e2ea1b
selfcan <99958756+selfcan@users.noreply.github.com>
2023-02-14 08:15:15 +0800

    Update immortalwrt.sh

--- diy/immortalwrt.sh
+++ diy/immortalwrt.sh
@@ -22,7 +22,8 @@ git clone https://github.com/selfcan/luci-app-adbyby-plus-lite.git package/luci-
 #添加luci-app-netspeedtest
 git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
 #添加 luci-app-my-dnshelper
-git clone https://github.com/selfcan/openwrt-my-dnshelper.git package/luci-app-my-dnshelper
+#git clone https://github.com/selfcan/openwrt-my-dnshelper.git package/luci-app-my-dnshelper
+git clone https://github.com/kongfl888/openwrt-my-dnshelper.git package/luci-app-my-dnshelper
 #添加luci-app-filebrowser
 git clone https://github.com/tcsr200722/luci-app-filebrowser.git package/luci-app-filebrowser
 #git clone https://github.com/openwrt-xiaomi/luci-app-filebrowser.git package/luci-app-filebrowser
