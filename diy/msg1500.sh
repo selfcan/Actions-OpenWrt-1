76ee988261dca8bc5df8f8639c23863b37d70631
hy5528 <95360525+hy5528@users.noreply.github.com>
2023-05-15 23:39:35 +0800

    Update msg1500.sh

--- diy/msg1500.sh
+++ diy/msg1500.sh
@@ -16,6 +16,5 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 sed -i 's/Bootstrap/Argon/g' feeds/luci/collections/luci/Makefile
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# max conntrack
-sed -i 's,16384,65536,g' package/kernel/linux/files/sysctl-nf-conntrack.conf
+
 
